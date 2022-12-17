e1cd147f1c76879ecc51fde26799902ed1a7e3be
UtsavBalar1231 <utsavbalar1231@gmail.com>
2022-12-17 12:38:02 +0530

    drm: legacy_modeset: Refactor to send FpgaFrame to FPGA

--- examples/legacy_modeset.rs
+++ examples/legacy_modeset.rs
@@ -1,14 +1,38 @@
+extern crate ascii_converter;
 extern crate drm;
 extern crate image;
 
 mod utils;
+
 use utils::*;
 
 use drm::control::Device as ControlDevice;
 
 use drm::buffer::DrmFourcc;
 
+use ascii_converter::string_to_decimals;
 use drm::control::{connector, crtc};
+
+struct HexSlice<'a>(&'a [u8]);
+
+impl<'a> HexSlice<'a> {
+    fn new<T>(data: &'a T) -> HexSlice<'a>
+    where
+        T: ?Sized + AsRef<[u8]> + 'a,
+    {
+        HexSlice(data.as_ref())
+    }
+}
+
+impl std::fmt::Display for HexSlice<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for b in self.0 {
+            write!(f, "{:#04X} ", b)?;
+        }
+
+        Ok(())
+    }
+}
 
 pub fn main() {
     let card = Card::open_global();
@@ -43,40 +67,58 @@ pub fn main() {
     let crtc = crtcinfo.get(0).expect("No crtcs found");
 
     // Select the pixel format
-    let fmt = DrmFourcc::Xrgb8888;
+    let fmt = DrmFourcc::Rgb888;
 
     // Create a DB
     // If buffer resolution is larger than display resolution, an ENOSPC (not enough video memory)
     // error may occur
     let mut db = card
-        .create_dumb_buffer((disp_width.into(), disp_height.into()), fmt, 32)
+        .create_dumb_buffer((disp_width.into(), disp_height.into()), fmt, 24)
         .expect("Could not create dumb buffer");
 
     // Map it and grey it out.
     {
+        let args_data = std::env::args().collect::<Vec<String>>();
+        let mut data: Vec<u8> = vec![
+            0xEA, 0xFF, 0x00, 0x00, 0x00, 0x01, 0x01, 0x00, 0x00, 0x00, 0x06, 0x00,
+        ];
+
+        // replace 11th element for data length
+        data[10] = (args_data[1].len()) as u8;
+
+        // convert string vector to decimal ascii
+        let mut temp = string_to_decimals(&args_data[1]).unwrap();
+        data.append(&mut temp);
+
+        let hexdata = HexSlice::new(&data);
+        println!("data: {}", hexdata);
+
         let mut map = card
             .map_dumb_buffer(&mut db)
             .expect("Could not map dumbbuffer");
         for b in map.as_mut() {
-            *b = 128;
+            for e in data.iter() {
+                *b = *e;
+            }
         }
     }
 
     // Create an FB:
     let fb = card
-        .add_framebuffer(&db, 24, 32)
+        .add_framebuffer(&db, 24, 24)
         .expect("Could not create FB");
 
-    println!("{:#?}", mode);
-    println!("{:#?}", fb);
-    println!("{:#?}", db);
+    println!("{:?}", mode);
+    println!("{:?}", fb);
+    println!("{:?}", db);
+    println!("{:?}", crtc.handle());
 
     // Set the crtc
     // On many setups, this requires root access.
     card.set_crtc(crtc.handle(), Some(fb), (0, 0), &[con.handle()], Some(mode))
         .expect("Could not set CRTC");
 
-    let five_seconds = ::std::time::Duration::from_millis(5000);
+    let five_seconds = ::std::time::Duration::from_millis(2000);
     ::std::thread::sleep(five_seconds);
 
     card.destroy_framebuffer(fb).unwrap();
